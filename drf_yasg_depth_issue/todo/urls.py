2a51fde18e995e833b345f147d2f24da0f034a06
dilipMaharjan <dilipmaharjann@gmail.com>
2018-02-21 16:27:43 -0500

    issue with doc

---
+++ drf_yasg_depth_issue/todo/urls.py
@@ -0,0 +1,16 @@
+import todo.views as views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'todo/a', views.AViewSet)
+
+router.register(r'todo/p', views.PViewSet)
+router.register(r'todo/c', views.CViewSet)
+router.register(r'todo/chu', views.ChuViewSet)
+router.register(r'todo/t', views.TViewSet)
+router.register(r'todo/co', views.CoViewSet)
+router.register(r'todo/tf', views.TfViewSet)
+urlpatterns = [
+]
+
+urlpatterns += router.urls
